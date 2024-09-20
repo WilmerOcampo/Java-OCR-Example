1d4ca68336a1291f2450fc8af16020b32ed00a29
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-09-20 12:04:02 -0500

    Updating Main class

--- src/main/java/com/wo/Main.java
+++ src/main/java/com/wo/Main.java
@@ -4,7 +4,20 @@ import com.wo.service.OCRService;
 
 public class Main {
     public static void main(String[] args) {
-        // Option 1: all inside in the main
+        // Option 1 (Recommended): using a service
+        OCRService ocrService = new OCRService();
+        String imagePath = "D:\\Tess4j\\example1.png"; // Image path
+        String outputPath = "D:\\Tess4j\\example.txt"; // Exit route
+
+        String text = ocrService.processImage(imagePath, outputPath); // Process the image
+
+        if (text != null) {
+            System.out.println("OCR completed successfully.");
+        } else {
+            System.out.println("The OCR process failed.");
+        }
+
+        // Option 2: all inside in the main
         /*ITesseract tesseract = new Tesseract();
         try {
             // If Tesseract-OCR would be installed in the path: D:\\Tesseract-OCR\\tessdata
@@ -23,20 +36,6 @@ public class Main {
             System.out.println("Text saved!");
         } catch (TesseractException | IOException e) {
             System.out.println("Error during OCR processing: " + e.getMessage() + ' ' + e);
-        }
-        */
-
-        // Option 2 (Recommended): using a service
-        OCRService ocrService = new OCRService();
-        String imagePath = "D:\\Tess4j\\example1.png"; // Image path
-        String outputPath = "D:\\Tess4j\\example.txt"; // Exit route
-
-        String text = ocrService.processImage(imagePath, outputPath); // Process the image
-
-        if (text != null) {
-            System.out.println("OCR completed successfully.");
-        } else {
-            System.out.println("The OCR process failed.");
-        }
+        }*/
     }
 }
